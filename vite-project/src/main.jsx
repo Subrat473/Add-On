79685993a130464bdea000886620c21d797438ae
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-03-01 17:42:23 +0000

    Counter

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-
-function MyApp() {
-  return (
-    <div>
-      <h1>Hello to all</h1>
-    </div>
-  )
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
